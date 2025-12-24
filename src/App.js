6872ed2129c4c4044a1cf6136bac9c01de29f303
Arman Shaikh <22071120@ycce.in>
2025-12-24 22:40:59 +0530

    Switch to HashRouter for GitHub Pages routing fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Textform from './components/Textform';
 import Alert from './components/Alert';
 import About from './components/About';
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light');
